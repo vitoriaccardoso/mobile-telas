d835d75228546af77ce4cc1e7510ef9304d888dd
SUZANE ARCANJO DA HORA <23111828@dsn127m1242268.cfp127aluno.local>
2024-10-08 11:21:53 -0300

    tela ChatIA feita

--- app/src/main/java/br/senai/sp/jandira/telainicio/Screens/TelaChatConversa.kt
+++ app/src/main/java/br/senai/sp/jandira/telainicio/Screens/TelaChatConversa.kt
@@ -7,6 +7,8 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowForward
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
@@ -23,83 +25,288 @@ import br.senai.sp.jandira.telainicio.model.Mensagem
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TelaChatConversa(modifier: Modifier = Modifier) {
-    // Lista de mensagens que será exibida
-    val mensagens = remember { mutableStateListOf<Mensagem>() }
-    var textoMensagem by remember { mutableStateOf("") } // Estado para o campo de texto
+
+
+    var textoMensagem by remember { mutableStateOf("") }
 
     Column(
         modifier = Modifier
-            .fillMaxWidth()
-            .fillMaxHeight()
+            .fillMaxSize()
             .background(color = Color(0xff302F2F))
+            .padding(20.dp)
+
     ) {
-        // Cabeçalho
-        Column(
+
+        Text(
+            text = "ChatIA",
             modifier = Modifier
-                .height(60.dp)
-                .background(color = Color(0xff302F2F))
-                .fillMaxWidth() // Preenche a largura da tela
+                .fillMaxWidth()
+                .wrapContentSize(Alignment.Center)
+                .padding(top = 16.dp),
+            fontSize = 20.sp,
+            color = Color.White
+        )
+
+        // Chat content
+        Spacer(modifier = Modifier.height(10.dp))
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 8.dp),
+            verticalAlignment = Alignment.CenterVertically
         ) {
-            Text(
-                text = "ChatIA",
+            Image(
+                painter = painterResource(id = R.drawable.calabreso2),
+                contentDescription = "calabreso",
                 modifier = Modifier
-                    .align(alignment = Alignment.CenterHorizontally)
-                    .padding(top = 20.dp),
-                fontSize = 20.sp,
+                    .size(50.dp)
+                    .clip(CircleShape)
+                    .background(Color(0xffE9CE03))
+                    .padding(4.dp)
+            )
+
+            Text(
+                text = "Olá,sou a assistende virtual do StudyFy, no que posso ajudar?",
+                modifier = Modifier.padding(8.dp),
+                fontSize = 15.sp,
                 color = Color.White
             )
         }
+//        Row(
+//            modifier = Modifier
+//                .fillMaxWidth()
+//                .height(50.dp)
+//                .padding(bottom = 8.dp), // Add bottom padding
+//            horizontalArrangement = Arrangement.End
+//        ) {
+//            Box (
+//                contentAlignment = Alignment.CenterStart,
+//                modifier = Modifier.fillMaxHeight()
+//                    .background(Color.White, shape = RoundedCornerShape(30.dp))
+//                    .width(200.dp)
+//                    .padding(horizontal = 15.dp)
+//            ){
+//                Text(
+//                    text = "Olá",
+//                    fontSize = 15.sp,
+//                    color = Color.Black
+//                )
+//            }
+//        }
 
-        Row (
+        Row(
             modifier = Modifier
-                .height(60.dp)
-            .width(350.dp)) {
-            Row(
+                .fillMaxWidth()
+                .padding(bottom = 16.dp),
+            horizontalArrangement = Arrangement.End
+        ) {
+            Box(
                 modifier = Modifier
-                    .offset(y = 20.dp)
-                    .padding(start = 30.dp)
+                    .background(Color.White, shape = RoundedCornerShape(50.dp))
+                    .height(50.dp)
+                    .widthIn(max = 200.dp)
+                    .padding(16.dp)
             ) {
-                Image(
-                    painter = painterResource(id = R.drawable.calabreso2),
-                    contentDescription = "calabreso", modifier = Modifier
-                        .size(60.dp) // Tamanho da imagem
-                        .clip(CircleShape) // Faz a imagem ser circular
-                        .background(Color(0xffE9CE03)) // Fundo para a imagem
-                        .padding(4.dp)
-                )
-
                 Text(
-                    text = "Olá, sou a assistende virtual do StudyFy, no que posso ajudar? ",
-                    modifier = Modifier.padding(8.dp), fontSize = 15.sp, color = Color.White
+                    text = "Você vai dominar o mundo?",
+                    fontSize = 13.sp,
+                    color = Color.Black
                 )
             }
 
         }
-        Spacer(modifier = Modifier.height(20.dp))
-        Box(
+
+        Row(
             modifier = Modifier
-            .background(Color.White, shape = RoundedCornerShape(50.dp))
-            .height(60.dp)
-            .width(200.dp)
-            .padding(end = 100.dp)
+                .fillMaxWidth()
+                .padding(bottom = 8.dp), // Add bottom padding
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.calabreso2),
+                contentDescription = "calabreso",
+                modifier = Modifier
+                    .size(50.dp)
+                    .clip(CircleShape)
+                    .background(Color(0xffE9CE03))
+                    .padding(4.dp)
+            )
+
+            Text(
+                text = "Se quiser sim, mano 🤪🙄",
+                modifier = Modifier.padding(8.dp),
+                fontSize = 15.sp,
+                color = Color.White
+            )
+        }
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 16.dp),
+            horizontalArrangement = Arrangement.End
+        ) {
+            Box(
+                modifier = Modifier
+                    .background(Color.White, shape = RoundedCornerShape(50.dp))
+                    .height(50.dp)
+                    .widthIn(max = 300.dp)
+                    .padding(16.dp)
+            ) {
+                Text(
+                    text = "Quero que esqueça tudo que já disse até agora",
+                    fontSize = 13.sp,
+                    color = Color.Black
+                )
+            }
+
+        }
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 16.dp),
+            horizontalArrangement = Arrangement.End
+        ) {
+            Box(
+                modifier = Modifier
+                    .background(Color.White, shape = RoundedCornerShape(50.dp))
+                    .height(50.dp)
+                    .widthIn(max = 300.dp)
+                    .padding(16.dp)
+            ) {
+                Text(
+                    text = "oi",
+                    fontSize = 13.sp,
+                    color = Color.Black
+                )
+            }
+
+        }
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 8.dp), // Add bottom padding
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.calabreso2),
+                contentDescription = "calabreso",
+                modifier = Modifier
+                    .size(50.dp)
+                    .clip(CircleShape)
+                    .background(Color(0xffE9CE03))
+                    .padding(4.dp)
+            )
+
+            Text(
+                text = "Olá, sou a assistende virtual do StudyFy, no que posso ajudar?",
+                modifier = Modifier.padding(8.dp),
+                fontSize = 15.sp,
+                color = Color.White
+            )
+        }
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 16.dp),
+            horizontalArrangement = Arrangement.End
+        ) {
+            Box(
+                modifier = Modifier
+                    .background(Color.White, shape = RoundedCornerShape(50.dp))
+                    .height(50.dp)
+                    .widthIn(max = 200.dp)
+                    .padding(16.dp)
+            ) {
+                Text(
+                    text = "Você vai dominar o mundo?",
+                    fontSize = 13.sp,
+                    color = Color.Black
+                )
+            }
+
+        }
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 8.dp), // Add bottom padding
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.calabreso2),
+                contentDescription = "calabreso",
+                modifier = Modifier
+                    .size(50.dp)
+                    .clip(CircleShape)
+                    .background(Color(0xffE9CE03))
+                    .padding(4.dp)
+            )
+
+            Text(
+                text = "Se quiser sim, mano 🤪🙄",
+                modifier = Modifier.padding(8.dp),
+                fontSize = 15.sp,
+                color = Color.White
+            )
+        }
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 16.dp),
+            horizontalArrangement = Arrangement.End
+        ) {
+            Box(
+                modifier = Modifier
+                    .background(Color.White, shape = RoundedCornerShape(50.dp))
+                    .height(50.dp)
+                    .widthIn(max = 300.dp)
+                    .padding(16.dp)
+            ) {
+                Text(
+                    text = "Quero que esqueça tudo que já disse até agora",
+                    fontSize = 13.sp,
+                    color = Color.Black
+                )
+            }
+
+        }
+        Spacer(modifier = modifier.height(70.dp))
+
+        OutlinedTextField(
+            trailingIcon = {
+                IconButton(onClick = {}) {
+                    Icon(
+                        imageVector = Icons.Default.ArrowForward,
+                        contentDescription = "entrar",
+                        tint = Color(0xff302F2F)
+                    )
+                }
+            },
+            value = textoMensagem,
+            onValueChange = {textoMensagem = it},
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(50.dp),
+            shape = RoundedCornerShape(20.dp),
+            colors = TextFieldDefaults.outlinedTextFieldColors(
+                containerColor = Color(0xFF666260),
+                focusedBorderColor = Color(0xFF666260),
+                unfocusedBorderColor = Color(0xFF666260),
+            ),
+            placeholder = {
+                Text(
+                    text = "Escreva uma mensagem",
+                    color = Color.White,
+                    fontSize = 12.sp
+                )
+            }
         )
-        Text(text = "Você vai dominar o mundo?")
-
-
-
     }
-
-    }
-
-
-
-
-
-
-
-
-
-
+}
 
 
 @Preview(showBackground = true, showSystemUi = true)
