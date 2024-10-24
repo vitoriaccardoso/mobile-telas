8e7394bde2a17399fbf987124e45c313abe4deb8
SUZANE ARCANJO DA HORA <23111828@dsn127m1246728.cfp127aluno.local>
2024-10-24 10:59:18 -0300

    font

---
+++ app/src/main/java/br/senai/sp/jandira/telainicio/ui/theme/Font.kt
@@ -0,0 +1,14 @@
+package br.senai.sp.jandira.telainicio.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import br.senai.sp.jandira.telainicio.R
+
+val poppinsFontFamily = FontFamily(
+    Font(R.font.poppins_thin, FontWeight.Thin),
+    Font(R.font.poppins_regular, FontWeight.Normal),
+    Font(R.font.poppins_medium, FontWeight.Medium),
+    Font(R.font.poppins_bold, FontWeight.Bold)
+
+)
